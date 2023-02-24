babd224214654eea245dd6ddc883b91ef22b9470
Rian C <93970224+eriancoet@users.noreply.github.com>
2023-02-24 10:21:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ $("nav").fadeIn("5000")
  
 });
 
-  function animatePictures() {
-jQuery('img').each(function(i) {
-  jQuery(this).delay(i * 300).fadeIn(3000).slideDown();
-});
-  }
-jQuery(window).load(function($) { animatePictures(); });
-    
+$(document).ready(function(){
+  $("img").fadeIn("5000")
+   
+  });
+
+
