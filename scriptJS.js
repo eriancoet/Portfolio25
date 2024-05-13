7dd1503b7fc65ab92f34865104a349944690ba68
Rian Coetzee <eriancoet@gmail.com>
2024-05-13 15:40:08 +0200

    update may 13 15:40

--- scriptJS.js
+++ scriptJS.js
@@ -67,3 +67,36 @@ function showDiv(index) {
     // Update the currentVisibleIndex
     currentVisibleIndex = index;
 }
+
+// slide of the portfolio-container
+
+
+// Get the portfolio container element
+var portfolioContainer = document.getElementById("portfolio-container");
+
+// Function to check if any part of the element is in the viewport
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    // Adjust the threshold to consider the top or bottom edge of the SVG
+    return (
+        (rect.top >= 0 && rect.top <= window.innerHeight) ||
+        (rect.bottom >= 0 && rect.bottom <= window.innerHeight)
+    );
+}
+
+
+
+// Function to handle scroll event
+function onScroll() {
+    if (isInViewport(portfolioContainer)) {
+        // Add a class to trigger the animation
+        portfolioContainer.classList.add("show");
+        // Remove the scroll event listener once the animation is triggered
+        window.removeEventListener("scroll", onScroll);
+    }
+}
+
+// Add scroll event listener
+window.addEventListener("scroll", onScroll);
+
+
